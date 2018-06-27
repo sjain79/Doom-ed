248f0ec35f0506565f86688c80a8b57b2990cff7
Unknown <sjain@student.fullsail.edu>
2018-06-26 20:54:30 -0400

    Enemy animations achieved

---
+++ Doom-ed/Assets/EnemyMovementScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovementScript : MonoBehaviour
+{
+    Vector2 startPosition;
+
+    private void Start()
+    {
+        startPosition = (Vector2)transform.position;
+    }
+
+    private void FixedUpdate()
+    {
+        transform.position = startPosition + new Vector2(Mathf.Sin(Time.time), 0.0f);
+    }
+}
